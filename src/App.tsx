6112b95537a349980b5ed49fe19d12a1d218d0d6
Kritsanaphong <kritsanaphong-keang@hotmail.com>
2021-09-09 14:05:23 +0700

    Complete

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,27 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { comments, CommentType } from './data/comments';
 
 function App() {
+  const generateReplies = (replies: CommentType[]) => {
+    return (replies.map(x =>
+      <div className="flex p-2 items-start space-x-2 pl-14">
+        <img className="w-10 w-10 rounded-full" src={x.userImagePath}></img>
+        <div className="bg-gray-500 rounded-lg p-2">
+          <p className="font-semibold text-white">{x.username}</p>
+          <p className='text-white'>{x.commentText}</p>
+          {x.likeNum > 0 ?
+            <div className='flex items-center'>
+              <img className='w-4 h-4 mr-1' src='/like.svg'></img>
+              <p className='text-gray-300'>{x.likeNum}</p>
+            </div> : null}
+        </div>
+      </div>
+    ))
+  }
+
+
   return (
     <div className="p-2">
       {/* post container */}
@@ -13,8 +32,8 @@ function App() {
 
           {/* image and name */}
           <div className="flex space-x-2 items-center">
-            <img className="w-12 h-12 rounded-full" src="/profileImages/handsome.jpg"></img>
-            <span className='font-semibold text-lg text-white'>Chayanin Suatap 610631100</span>
+            <img className="w-12 h-12 rounded-full" src="/profileImages/90221500_3358617120820605_4912083933849452544_n.png"></img>
+            <span className='font-semibold text-lg text-white'>Kritsanaphong Tepwerakul 630610714</span>
           </div>
 
           {/* status message */}
@@ -30,30 +49,26 @@ function App() {
 
         {/* comments section */}
         <div className="">
-
-          {/* normal comment */}
-          <div className="flex p-2 items-start space-x-2">
-            <img className="w-10 w-10 rounded-full" src="/profileImages/lisa.jpg"></img>
-            <div className="bg-gray-500 rounded-lg p-2">
-              <p className="font-semibold text-white">Lisa</p>
-              <p className='text-white'>ตัวอย่าง Template จ้า</p>
-              {/* like section (จะไม่แสดงถ้าไม่มีใครไลค์เลย) */}
-              <div className='flex items-center'>
-                <img className='w-4 h-4 mr-1' src='/like.svg'></img>
-                <p className='text-gray-300'>999 คน</p>
+          {comments.map(x =>
+            <div>
+              <div className="flex p-2 items-start space-x-2">
+                <img className="w-10 w-10 rounded-full" src={x.userImagePath}></img>
+                <div className="bg-gray-500 rounded-lg p-2">
+                  <p className="font-semibold text-white">{x.username}</p>
+                  <p className='text-white'>{x.commentText}</p>
+                  {/* like section (จะไม่แสดงถ้าไม่มีใครไลค์เลย) */}
+                  {x.likeNum > 0 ?
+                    <div className='flex items-center'>
+                      <img className='w-4 h-4 mr-1' src='/like.svg'></img>
+                      <p className='text-gray-300'>{x.likeNum}</p>
+                    </div> : null}
+                </div>
               </div>
+              {generateReplies(x.replies)}
             </div>
-          </div>
+          )}
 
-          {/* replies */}
-          {/* ต่างกันตรงที่มี padding มากกว่าเท่านั้น (pl-14) */}
-          <div className="flex p-2 items-start space-x-2 pl-14">
-            <img className="w-10 w-10 rounded-full" src="/profileImages/puppy.jpg"></img>
-            <div className="bg-gray-500 rounded-lg p-2">
-              <p className="font-semibold text-white">หมาน้อย</p>
-              <p className='text-white'>เม้นค้าบ</p>
-            </div>
-          </div>
+
 
         </div>
 
